ea570eb6cdc88f25f7d2e723e551e08de9b859f6
fradere8 <kulikovaos2845@gmail.com>
2026-01-18 00:55:22 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,8 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 
 export default defineConfig({
-  base: './',
-  root: '.', 
-  build: {
-    outDir: 'dist',     
-    emptyOutDir: true,  
-    rollupOptions: {
-      input: resolve(__dirname, 'src/scripts/index.js'), 
-    },
-  },
   server: {
-    open: true,         
+    open: true,
   },
+  base: './',
 });
